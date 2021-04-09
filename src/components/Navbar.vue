fab9181989150cfc92cc99802fd88ab51a535523
All PGS <kms0219kms@naver.com>
2021-04-09 15:43:15 +0900

    init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark navbar-setting">
+        <div class="container"><router-link class="navbar-brand" to="/"><img class="logo" alt="logo" src="/logo1024.png" height="30px" /><span class="navbar-main">Promcord</span></router-link><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon" /></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item"><router-link exact class="nav-link" to="/">홈 <span class="sr-only">(current)</span></router-link></li>
+                <li class="nav-item"><router-link class="nav-link" to="/about">소개 <span class="sr-only">(current)</span></router-link></li>
+                <li class="nav-item"><a class="nav-link" href="https://discord.com/api/oauth2/authorize?client_id=720567998783684631&permissions=8&scope=bot">초대 <span class="sr-only">(current)</span></a></li>
+                <li class="nav-item"><router-link class="nav-link" to="/guide">가이드 <span class="sr-only">(current)</span></router-link></li>
+                <li class="nav-item"><a class="nav-link" href="https://discord.gg/2tbXMMF">디스코드 <span class="sr-only">(current)</span></a></li>
+            </ul>
+            </div>
+        </div>
+      </nav>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
