160ede47999c61f8448b8cb716c1c474f3681a68
All PGS <kms0219kms@naver.com>
2021-04-10 22:51:57 +0900

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <div class="index-full">
+          <span class="index-topic">Promcord</span>
+          <span class="index-desc">Promcord는 자신의 디스코드 서버를 홍보하는 디스코드 봇입니다.<br />Promcord는 여러분의 디스코드 서버를 더 활기차게 만들어 드리려고 노력중입니다.</span>
+          <a href="https://discord.com/api/oauth2/authorize?client_id=720567998783684631&amp;permissions=8&amp;scope=bot" class="index-button">INVITE NOW</a>
+      </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import from '@/components/.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
