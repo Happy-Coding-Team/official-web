aab0d61259c7ecbbee652e804461f8ec9c6b63a1
All PGS <kms0219kms@naver.com>
2021-04-11 21:43:51 +0900

    init

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nav class="navbar navbar-expand-lg navbar-dark navbar-setting">
-        <div class="container"><router-link class="navbar-brand" to="/"><img class="logo" alt="logo" src="/logo630.png" height="30px" /><span class="navbar-main">즐거운 코딩팀</span></router-link><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon" /></button>
+        <div class="container"><router-link class="navbar-brand" to="/"><img class="logo" alt="logo" src="/logo.png" height="30px" /><span class="navbar-main">즐거운 코딩팀</span></router-link><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon" /></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item"><router-link exact class="nav-link" to="/">홈 <span class="sr-only">(current)</span></router-link></li>
