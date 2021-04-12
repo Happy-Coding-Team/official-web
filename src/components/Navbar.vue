79b335646b513a95b03c87d8e1053e424af5077a
All PGS <kms0219kms@naver.com>
2021-04-12 19:03:26 +0900

    init

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-dark navbar-setting">
-        <div class="container"><router-link class="navbar-brand" to="/"><img class="logo" alt="logo" src="/logo.png" height="30px" /><span class="navbar-main">즐거운 코딩팀</span></router-link><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon" /></button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item"><router-link exact class="nav-link" to="/">홈 <span class="sr-only">(current)</span></router-link></li>
-                <li class="nav-item"><router-link class="nav-link" to="/members">팀원 소개 <span class="sr-only">(current)</span></router-link></li>
-                <!-- <li class="nav-item"><router-link class="nav-link" to="#">프로젝트 이름 <span class="sr-only">(current)</span></a></li> -->
-                <!-- <li class="nav-item"><router-link class="nav-link" to="#">프로젝트 이름 <span class="sr-only">(current)</span></a></li> -->
-                <!-- <li class="nav-item"><router-link class="nav-link" to="#">프로젝트 이름 <span class="sr-only">(current)</span></a></li> -->
-                <!-- <li class="nav-item"><router-link class="nav-link" to="#">프로젝트 이름 <span class="sr-only">(current)</span></a></li> -->
-                <li class="nav-item"><router-link class="nav-link" to="/forum">포럼 <span class="sr-only">(current)</span></router-link></li>
-                <li class="nav-item"><a class="nav-link" href="/Career.html">팀원모집 <span class="sr-only">(current)</span></a></li>
-            </ul>
-            </div>
-        </div>
-      </nav>
+      <nav class="navbar navbar-expand-lg navbar-dark navbar-setting">
+          <div class="container"><router-link class="navbar-brand" to="/"><img class="logo" alt="logo" src="/logo.png" height="30px" /><span class="navbar-main">즐거운 코딩팀</span></router-link><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon" /></button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ml-auto">
+                  <li class="nav-item"><router-link exact class="nav-link" to="/">홈 <span class="sr-only">(current)</span></router-link></li>
+                  <li class="nav-item"><router-link class="nav-link" to="/members">팀원 소개 <span class="sr-only">(current)</span></router-link></li>
+                  <!-- <li class="nav-item"><router-link class="nav-link" to="#">프로젝트 이름 <span class="sr-only">(current)</span></a></li> -->
+                  <!-- <li class="nav-item"><router-link class="nav-link" to="#">프로젝트 이름 <span class="sr-only">(current)</span></a></li> -->
+                  <!-- <li class="nav-item"><router-link class="nav-link" to="#">프로젝트 이름 <span class="sr-only">(current)</span></a></li> -->
+                  <!-- <li class="nav-item"><router-link class="nav-link" to="#">프로젝트 이름 <span class="sr-only">(current)</span></a></li> -->
+                  <li class="nav-item"><router-link class="nav-link" to="/forum">포럼 <span class="sr-only">(current)</span></router-link></li>
+                  <li class="nav-item"><a class="nav-link" href="https://career.happy-coding.kro.kr/">팀원모집 <span class="sr-only">(current)</span></a></li>
+              </ul>
+              </div>
+          </div>
+        </nav>
     </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -22,14 +22,6 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "Forum" */ '../views/Forum.vue')
-  },
-  {
-    path: '/career',
-    name: 'Career',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "Career" */ '../views/Career.vue')
   }
 ]
 
