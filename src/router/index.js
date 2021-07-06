87f91623832518ab158639b3d808d107be2b42e6
All PGS <kms0219kms@suwondasanm.goe.go.kr>
2021-07-07 08:16:08 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+// import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
+// import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
 const routes = [
@@ -38,7 +40,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
