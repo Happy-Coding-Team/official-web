6ffc030291cdf3783903d592d2ed9c0b88dd3cb7
SSKATE <kms0219kms@suwondasanm.goe.go.kr>
2021-07-27 20:19:59 +0900

    Update Projects.vue

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -6,6 +6,17 @@
     </div>
   </div>
   <div class="introduce bg-1 cr-0">
+    <div class="container">
+        <div class="card-columns">
+            <!-- 경고! <br>&nbsp; 등의 문구는 사용 금지 (사용시 css 깨짐) -->
+            <div class="card bot-card">
+                <div class="card-body"><img class="bot-card-img" src="/static/assets/img/bots/minigame.png" alt="미니게임"><span class="card-title bot-card-title">미니게임</span>
+                    <p class="card-text bot-card-text"></p>
+                    <hr class="rounded bot-dividers"><a href="javascript:alert('아직 작업 중입니다.');" class="bot-button btn btn-info">Website</a><a href="javascript:alert('아직 작업 중입니다.');" class="bot-button btn btn-info">초대하기</a>
+                </div>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
